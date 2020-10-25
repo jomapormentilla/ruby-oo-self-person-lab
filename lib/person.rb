b62ea4d6f1e2de4996331c9f8276e4c7f0adce46
jomapormentilla <jomapormentilla@gmail.com>
2020-10-25 19:39:48 -0400

    commit tree 5958ea2398c8111448f3ec961bbd345e281b2231

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,82 @@
 # your code goes here
+
+class Person
+    attr_accessor   :bank_account, :happiness, :hygiene
+    attr_reader :name
+
+    def initialize( name )
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness=( new_value )
+        if new_value > 10
+            @happiness = 10
+        elsif new_value < 0
+            @happiness = 0
+        else
+            @happiness = new_value
+        end
+    end
+
+    def hygiene=( new_value )
+        if new_value > 10
+            @hygiene = 10
+        elsif new_value < 0
+            @hygiene = 0
+        else
+            @hygiene = new_value
+        end
+    end
+
+    def clean?
+        @hygiene > 7 ? true : false
+    end
+
+    def happy?
+        @happiness > 7 ? true : false
+    end
+
+    def get_paid( salary )
+        @bank_account += salary
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene=(@hygiene + 4)
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.happiness=(@happiness + 2)
+        self.hygiene=(@hygiene - 3)
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend( friend )
+        self.happiness=(@happiness + 3)
+        friend.happiness=(friend.happiness + 3)
+
+        if friend.name == "Felix"
+            "Hi Felix! It's Penelope. How are you?"
+        else
+            "Hi Penelope! It's Felix. How are you?"
+        end
+    end
+
+    def start_conversation( person, topic )
+        if topic == "politics"
+            self.happiness=(@happiness - 2)
+            person.happiness=(person.happiness - 2)
+            "blah blah partisan blah lobbyist"
+        elsif topic == "weather"
+            self.happiness=(@happiness + 1)
+            person.happiness=(person.happiness + 1)
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+end
